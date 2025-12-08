589e2c3caacab8b9b5fad709d142140fcf73d982
saransh <saranshgumasta@gmail.com>
2025-12-08 14:50:00 +0530

    now each file is updated

--- java/app.java
+++ java/app.java
@@ -1,6 +1,5 @@
-public class app {
+public class App {
     public static void main(String[] args) {
         System.out.println("Java Container Running Successfully!");
     }
 }
-## --------------------------------
